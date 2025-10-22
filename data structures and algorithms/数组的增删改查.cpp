783bd531626747ee5473c4289d0cbd8ba38afb74
俊 陈 <jun237193@gmail.com>
2025-10-23 00:38:46 +0800

    新

---
+++ data structures and algorithms/数组的增删改查.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Array {
+public:
+	Array(int size = 10):size(size),mCur(0) {
+		arr = new int[size];
+	}
+	void push_bak();
+	void pop_back();
+	void inster();
+	void erase();
+	void find();
+private:
+	int* arr; //数组指针
+	int size; //大小
+	int mCur; //当前元素个数
+
+};
+
+
+int main() {
+
+
+	return 0;
+
+}
